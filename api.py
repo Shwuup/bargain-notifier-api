2c14a95e0ed89743e7d5c0f6cb989583e571d501
Shwuup <e_kong98z@hotmail.com>
2019-07-18 22:56:52 +1000

    able to open unseen bargains from clicking on keywords
    test_bargains is a text file and not a html file because
    I didn't want it to skew the GitHub language details.

--- api.py
+++ api.py
@@ -8,14 +8,19 @@ api = hug.API(__name__)
 api.http.add_middleware(CORSMiddleware(api))
 
 
-def get_html_doc():
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0"
-    }
-    html_content = requests.get(
-        "https://www.ozbargain.com.au/", headers=headers
-    ).content
-    return BeautifulSoup(html_content, "html.parser")
+def get_html_doc(is_test=False):
+    if not is_test:
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0"
+        }
+        html_content = requests.get(
+            "https://www.ozbargain.com.au/", headers=headers
+        ).content
+        return BeautifulSoup(html_content, "html.parser")
+    else:
+        with open("test_bargains.txt") as file:
+            html_content = file.read()
+            return BeautifulSoup(html_content, "html.parser")
 
 
 @hug.post("/bargain")
@@ -24,10 +29,18 @@ def bargain(body, response):
     return get_elegible_bargains(body)
 
 
-def get_elegible_bargains(keyword_object):
-    soup = get_html_doc()
+@hug.post("/test_bargain")
+def test_bargain(body, response):
+    print(body)
+    return get_elegible_bargains(body, is_test=True)
+
+
+def get_elegible_bargains(keyword_object, is_test=False):
+    if is_test:
+        soup = get_html_doc(is_test=True)
+    else:
+        soup = get_html_doc()
     res = soup.find_all(class_="node-ozbdeal")
-    #TODO: need to think about how to tell when it is actually a NEW deal. and where that logic lives;
     for offer_html in res:
         offer_info = offer_html.find("h2", class_="title")
         for keyword in keyword_object["keywords"]:
@@ -35,13 +48,15 @@ def get_elegible_bargains(keyword_object):
                 "title"
             )
             offer_title = offer_info["data-title"]
-            seen_links = [offer[0] for offer in keyword_object["keywords"][keyword]["offers"]]
+            seen_links = [
+                offer[0] for offer in keyword_object["keywords"][keyword]["offers"]
+            ]
             if keyword.lower() in offer_title.lower() and link not in seen_links:
+                keyword_info = keyword_object["keywords"][keyword]
                 keyword_object["areThereNewDeals"] = True
                 print(offer_title)
-                offers = keyword_object["keywords"][keyword]["offers"]
-                offers.append([link, offer_title])
-                keyword_object["keywords"][keyword] = {"offers":offers, "hasUserClicked": False}
-                
+                keyword_info["offers"].append([link, offer_title])
+                keyword_info["isOnFrontPage"] = True
+                keyword_info["hasUserClicked"] = False
 
     return keyword_object
