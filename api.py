237bef8ab892856f691a7949f32ee3600df61a32
Shwuup <e_kong98z@hotmail.com>
2020-01-07 23:55:04 +1100

    simplify seendeals and pack html parsing into scheduled event

--- api.py
+++ api.py
@@ -4,25 +4,11 @@ import logging
 import hug
 import boto3
 from hug.middleware import CORSMiddleware
+import json
 
 
 api = hug.API(__name__)
 api.http.add_middleware(CORSMiddleware(api))
-
-
-def get_html_doc(is_test=False):
-    if not is_test:
-        headers = {
-            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0"
-        }
-        html_content = requests.get(
-            "https://www.ozbargain.com.au/", headers=headers
-        ).content
-        return BeautifulSoup(html_content, "html.parser")
-    else:
-        with open("test_bargains.txt") as file:
-            html_content = file.read()
-            return BeautifulSoup(html_content, "html.parser")
 
 
 @hug.post("/bargain")
@@ -42,36 +28,33 @@ def test_bargain(body, response):
     return get_elegible_bargains(body, is_test=True)
 
 
-def get_elegible_bargains(keyword_object, is_test=False):
-    if is_test:
-        soup = get_html_doc(is_test=True)
-    else:
-        # get cached copy from s3 bucket
-        s3 = boto3.resource("s3")
-        obj = s3.Object("bargain-notifier-bucket", "bargain_html")
-        html_content = obj.get()["Body"].read().decode("utf-8")
-        soup = BeautifulSoup(html_content, "html.parser")
-    res = soup.find_all(class_="node node-ozbdeal node-teaser")
+def get_cached_deals():
+    s3 = boto3.resource("s3")
+    obj = s3.Object("bargain-notifier-bucket", "dic")
+    deals = json.loads(obj.get()["Body"].read().decode("utf-8"))
+    return deals
 
-    for offer_html in res:
-        offer_info = offer_html.find("h2", class_="title")
-        for keyword in keyword_object["keywords"]:
-            keyword_info = keyword_object["keywords"][keyword]
-            link = "https://www.ozbargain.com.au/node/" + offer_info["id"].strip(
-                "title"
-            )
-            offer_title = offer_info["data-title"]
-            seen_links = [offer[0] for offer in keyword_info["offers"]]
-            if keyword.lower() in offer_title.lower() and link not in seen_links:
-                keyword_object["areThereNewDeals"] = True
-                print(offer_title)
-                keyword_info["offers"].append([link, offer_title])
+
+def get_elegible_bargains(payload):
+    cached_deals = get_cached_deals()
+    new_seen_deals = {}
+    for link, title in cached_deals.items():
+        for keyword in payload["keywords"]:
+            keyword_info = payload["keywords"][keyword]
+            if keyword in title.lower() and link not in payload["seenDeals"]:
+                payload["areThereNewDeals"] = True
+                print(title)
+                keyword_info["offers"][link] = title
                 if (
                     not keyword_info["isOnFrontPage"]
                     and not keyword_info["hasUserClicked"]
                 ) or keyword_info["hasUserClicked"]:
-                    keyword_object["numberOfUnclickedKeywords"] += 1
+                    payload["numberOfUnclickedKeywords"] += 1
                 keyword_info["isOnFrontPage"] = True
                 keyword_info["hasUserClicked"] = False
+    for url in payload["seenDeals"]:
+        if url not in cached_deals:
+            del payload["seenDeals"][url]
 
-    return keyword_object
+    return payload
+
--- scraper.py
+++ scraper.py
@@ -2,32 +2,35 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from bs4 import BeautifulSoup
 import requests
 import boto3
+import json
 
 
 sched = BlockingScheduler()
 
 
-def get_html_doc(is_test=False):
-    if not is_test:
-        headers = {
-            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0"
-        }
-        html_content = requests.get(
-            "https://www.ozbargain.com.au/", headers=headers
-        ).content
-        return BeautifulSoup(html_content, "html.parser").prettify()
-    else:
-        with open("test_bargains.txt") as file:
-            html_content = file.read()
-            return BeautifulSoup(html_content, "html.parser")
+def get_deals(url):
+    page = requests.get(url).content
+    soup = BeautifulSoup(page, "html.parser")
+    res = soup.find_all(class_="node node-ozbdeal node-teaser")
+    dic = {}
+    for elem in res:
+        h2 = elem.find("h2", class_="title")
+        link = "https://www.ozbargain.com.au/node/" + elem["id"].strip("node")
+        dic[link] = h2["data-title"]
+    return dic
+
+
+def upload():
+    deals = get_deals("https://www.ozbargain.com.au/")
+    new_deals = get_deals("https://www.ozbargain.com.au/deals")
+    deals.update(new_deals)
+    s3 = boto3.resource("s3")
+    s3.Bucket("bargain-notifier-bucket").put_object(Key="dic", Body=json.dumps(deals))
 
 
 @sched.scheduled_job("interval", minutes=25)
 def timed_job():
-    doc = get_html_doc()
-    # upload to s3 bucket
-    s3 = boto3.resource("s3")
-    s3.Bucket("bargain-notifier-bucket").put_object(Key="bargain_html", Body=doc)
+    upload()
 
 
 sched.start()
